494832f6f68940cdd6ad4d11cc2dbe9d9b2f3d46
Tor Håvard Bieltvedt <th@bieltvedt.net>
2013-07-05 22:18:45 +0200

    Upgraded to sbt-plugin 2.1.1.

--- project/Build.scala
+++ project/Build.scala
@@ -1,18 +1,16 @@
 import sbt._
 import Keys._
-import play.Project._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "font-theater"
-    val appVersion      = "1.1-SNAPSHOT"
+  val appName = "font-theater"
+  val appVersion = "1.1-SNAPSHOT"
 
-    val appDependencies = Seq(
-      "org.fusesource.scalate" % "scalate-core_2.10" % "1.6.1"
-    )
+  val appDependencies = Seq(
+    "org.fusesource.scalate" % "scalate-core_2.10" % "1.6.1"
+  )
 
-    val main = play.Project(appName, appVersion, appDependencies).settings(
-      // Add your own project settings here      
-    )
-
+  val main = play.Project(appName, appVersion, appDependencies).settings(
+    resolvers += Resolver.url("Font Theater Play Repository", url("http://thbieltvedt.github.com/releases-font-theater"))(Resolver.ivyStylePatterns)
+  )
 }
